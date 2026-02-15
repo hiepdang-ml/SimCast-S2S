05d7347de885103320bb7afe62674ac5e189d0b1
dangvohiep <hiepdangvo@icloud.com>
2026-02-15 03:17:23 -0500

    implemented training/inference pipeline for era5

--- models/diffusion/__init__.py
+++ models/diffusion/__init__.py
@@ -1,7 +1,12 @@
 from .vae import (
-    VAE, VAE_Wind, VAE_Mass, VAE_Thermal, VAE_Hydro, VAE_Precip, VAE_Target,
+    VAE, VAE_Wind, VAE_Mass, VAE_Thermal, VAE_Hydro, VAE_Precip,
     VAEEncoder, VAEDecoder
 )
 from .diffusion import (
     UNetDenoiser, LinearNoiseScheduler, CosineNoiseScheduler, ForwardProcess, ReverseProcess,
 )
+__all__ = [
+    "VAE", "VAE_Wind", "VAE_Mass", "VAE_Thermal", "VAE_Hydro", "VAE_Precip",
+    "VAEEncoder", "VAEDecoder",
+    "UNetDenoiser", "LinearNoiseScheduler", "CosineNoiseScheduler", "ForwardProcess", "ReverseProcess",
+]
