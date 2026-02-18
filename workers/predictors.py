f2554a829661c9e9e8446c81ed96690ed2492768
dangvohiep <hiepdangvo@icloud.com>
2026-02-18 00:10:19 -0500

    continue diffusion inference on era5

--- workers/predictors.py
+++ workers/predictors.py
@@ -98,7 +98,10 @@ class _AbstractPredictor(ABC):
         # Batch size should always be 1
         records: dict[str, list[torch.Tensor]] = {"predictions": [], "groundtruths": []}
         # Predict
-        for batch in self.dataloader:
+        # DEBUG
+        for i, batch in enumerate(self.dataloader):
+            if i < 82:
+                continue
             prediction_mean, groundtruth_mean = self._predict_step(batch=batch)
             # Record for aggregate metrics
             records["groundtruths"].append(groundtruth_mean)
