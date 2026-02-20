8fff403b91927b9ee65cd00b832fd6e8d71e6150
dangvohiep <hiepdangvo@icloud.com>
2026-02-19 23:08:47 -0500

    update checkpoint for vae-precip on era5

--- workers/predictors.py
+++ workers/predictors.py
@@ -98,10 +98,7 @@ class _AbstractPredictor(ABC):
         # Batch size should always be 1
         records: dict[str, list[torch.Tensor]] = {"predictions": [], "groundtruths": []}
         # Predict
-        # TODO
-        for i, batch in enumerate(self.dataloader):
-            if i < 95:
-                continue
+        for batch in self.dataloader:
             prediction_mean, groundtruth_mean = self._predict_step(batch=batch)
             # Record for aggregate metrics
             records["groundtruths"].append(groundtruth_mean)
