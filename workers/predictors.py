499f8b9bf84c2bda1fb0a81ab498d6b32516e517
dangvohiep <hiepdangvo@icloud.com>
2026-02-24 10:49:23 -0500

    continue diffusion with eta=080 trained cesm2 test era5

--- workers/predictors.py
+++ workers/predictors.py
@@ -99,7 +99,7 @@ class _AbstractPredictor(ABC):
         records: dict[str, list[torch.Tensor]] = {"predictions": [], "groundtruths": []}
         # Predict
         for i, batch in enumerate(self.dataloader):
-            if i < 40:
+            if i < 19:
                 continue
             prediction_mean, groundtruth_mean = self._predict_step(batch=batch)
             # Record for aggregate metrics
