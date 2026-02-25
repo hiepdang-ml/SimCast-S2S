528557236c88be966185b65fb741de58fbae3bcc
Hiep Dang <49779915+hiepdang-ml@users.noreply.github.com>
2026-02-25 08:48:36 -0500

    include checkpoints for cnn and unet models in config

--- workers/predictors.py
+++ workers/predictors.py
@@ -98,9 +98,7 @@ class _AbstractPredictor(ABC):
         # Batch size should always be 1
         records: dict[str, list[torch.Tensor]] = {"predictions": [], "groundtruths": []}
         # Predict
-        for i, batch in enumerate(self.dataloader):
-            if i < 28:
-                continue
+        for batch in self.dataloader:
             prediction_mean, groundtruth_mean = self._predict_step(batch=batch)
             # Record for aggregate metrics
             records["groundtruths"].append(groundtruth_mean)
