d5425a426bd1a8a58118be243ed8dc1763cc3829
Hiep Dang <49779915+hiepdang-ml@users.noreply.github.com>
2026-03-04 00:38:15 -0500

    work around to keep the params' names unchanged when finetuning

--- train.py
+++ train.py
@@ -448,7 +448,7 @@ def main(
             print(f"Training UNetDenoiser from {checkpoint_path}")
             checkpoint_loader = CheckpointLoader(checkpoint_path=str(checkpoint_path))
             net: UNetDenoiser = checkpoint_loader.load(
-                scope=globals(), plug_finetune_head=is_finetuning,
+                scope=globals(), is_finetuning=is_finetuning,
                 ignored_modules=["head.weight", "head.bias"] if is_finetuning else []   # these only exist in original diffusion
             )
             assert isinstance(net, UNetDenoiser)
