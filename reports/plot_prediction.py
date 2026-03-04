35ffa5851cd693b70cd60b8f013d9fa23fdfc014
Hiep Dang <49779915+hiepdang-ml@users.noreply.github.com>
2026-03-04 11:48:38 -0500

    re-examine VAEs trained on cesm2, tested on era5

--- reports/plot_prediction.py
+++ reports/plot_prediction.py
@@ -39,13 +39,13 @@ def main(
     visualizer = Visualizer(metadata=metadata, source_dir=source_dir, target_dir=target_dir)
     for f in sorted(visualizer.source_dir.glob("*.pt")):
         try:
+            print(f)
             if model in ["cnn", "unet", "vit"]:
                 visualizer.plot_baseline_prediction(f.name)
             elif model.startswith("vae"):
                 visualizer.plot_vae_prediction(f.name)
             else:
                 visualizer.plot_diffusion_prediction(f.name)
-                print(f)
         except RuntimeError:
             # Catch files only half-written due to KeyboardInterrupted inferences
             print(f"{f} (Skipped)")
